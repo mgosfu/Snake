383904fe388b2e0bc1628092be3f3c02df3c0a04
Martin Ouellet <moa21@sfu.ca>
2016-08-29 20:47:23 -0700

    Create snake game

---
+++ app/src/main/java/com/mgodevelopment/snake/GameSettings.java
@@ -0,0 +1,31 @@
+package com.mgodevelopment.snake;
+
+/**
+ * Created by Martin on 8/29/2016.
+ */
+public final class GameSettings {
+
+    public static final int FOOD_POINTS = 3; // points for eating food
+    public static final int NUMBER_POINTS = 20; // number of points in bomb mode
+    public static final int POINTS_BOMB_ANIMATION = 15; // animation in bomb mode
+    public static final int POINTS_ANIMATION = 4;
+    public static final int POISON_FOOD = 5;
+
+    public static final String MY_AD_UNIT_ID = "TBA";
+
+    // flash animation in ms for the app
+    public static final int ANIMATION_OPEN_BUTTON_DURATION = 400;
+    public static final int ANIMATION_CLOSE_BUTTON_DURATION = 400;
+    public static final int ANIMATION_SHOW_HOME_BUTTON_DURATION = 500;
+    public static final int ANIMATION_HIDE_HOME_BUTTON_DURATION = 500;
+    public static final int ANIMATION_SHOW_TITLE_DURATION = 400;
+    public static final int ANIMATION_HIDE_TITLE_DURATION = 400;
+
+    public static final int SHAKE_DURATION = 500;
+    public static final int GAME_THREAD = 80;
+    public static final int GAME_THREAD_BOMB = 60;
+    public static final int SWIPE_THRESH_HOLD = 100;
+    public static final int SWIPE_VELUCITY_THRESH_HOLD = 100;
+    public static final int LAYOUT_PADDDING = 0;
+
+}
