4e960d20274fb9e55b6a750fa16bb92e1445b21e
Martin Ouellet <moa21@sfu.ca>
2016-08-29 21:30:37 -0700

    188

--- app/src/main/java/com/mgodevelopment/snake/MainMenu.java
+++ app/src/main/java/com/mgodevelopment/snake/MainMenu.java
@@ -1,41 +1,123 @@
 package com.mgodevelopment.snake;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 
 public class MainMenu extends AppCompatActivity {
 
+    private RelativeLayout snakeLayout;
+    private Animation compileAnim;
+    private AdView adView;
+    private ImageView btnClassic;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.main_menu);
+        snakeLayout = (RelativeLayout) findViewById(R.id.snake_layout);
 
-    }
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
         }
 
-        return super.onOptionsItemSelected(item);
+        adView = new AdView(this);
+        adView.setAdSize(AdSize.SMART_BANNER);
+        adView.setAdUnitId(GameSettings.MY_AD_UNIT_ID);
+
+        snakeLayout.addView(adView);
+        AdRequest adRequest = new AdRequest.Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR).build();
+
+        adView.loadAd(adRequest);
+
     }
+
+    private void initClassic() {
+
+        btnClassic = (ImageView) findViewById(R.id.classic);
+        compileAnim = AnimationUtils.loadAnimation(MainMenu.this, R.anim.anim_for_classic_button);
+        compileAnim.setDuration(GameSettings.ANIMATION_OPEN_BUTTON_DURATION);
+        compileAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                btnClassic.setImageResource(R.mipmap.classic);
+                btnClassic.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        Intent intentClassic = new Intent(MainMenu.this, ClassicSnake.class);
+                        intentClassic.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                        startActivity(intentClassic);
+
+                    }
+                });
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        btnClassic.startAnimation(compileAnim);
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
