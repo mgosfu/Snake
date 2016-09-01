53f2e00ff30a0199f986cf8241c71276c7f8cf28
Martin Ouellet <moa21@sfu.ca>
2016-08-31 22:28:43 -0700

    193

--- app/src/main/java/com/mgodevelopment/snake/ClassicSnake.java
+++ app/src/main/java/com/mgodevelopment/snake/ClassicSnake.java
@@ -5,13 +5,32 @@ import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
 public class ClassicSnake extends AppCompatActivity {
 
     private boolean playMusic;
     private MediaPlayer musicPlayer;
+
+    private RelativeLayout classicSnakeLayout;
+    private boolean isInitialized;
+
+    private GestureDetector mGestureDetector;
+    private boolean isPaused;
+
+    private boolean isGoingLeft = false;
+    private boolean isGoingRight = false;
+    private boolean isGoingUp = false;
+    private boolean isGoingDown = false;
+
+    private boolean clickLeft;
+    private boolean clickRight;
+    private boolean clickUp;
+    private boolean clickDown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +42,18 @@ public class ClassicSnake extends AppCompatActivity {
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
         decorView.setSystemUiVisibility(uiOptions);
+
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
+
+        musicOnOff();
+
+        classicSnakeLayout = (RelativeLayout) findViewById(R.id.class_snake_layout);
+        classicSnakeLayout.setBackgroundResource(R.mipmap.background_for_snake);
+        classicSnakeLayout.setPaddingRelative(GameSettings.LAYOUT_PADDDING, GameSettings.LAYOUT_PADDDING, GameSettings.LAYOUT_PADDDING, GameSettings.LAYOUT_PADDDING);
+
+        isInitialized = false;
 
     }
 
@@ -43,6 +71,110 @@ public class ClassicSnake extends AppCompatActivity {
             musicPlayer.stop();
         }
 
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        return mGestureDetector.onTouchEvent(event) || super.onTouchEvent(event);
+
+    }
+
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+        isPaused = true;
+        musicPlayer.release();
+
+    }
+
+    private void onSwipeRight() {
+        if (!isGoingRight && !isGoingLeft) {
+
+            isGoingRight = true;
+            isGoingLeft = false;
+            isGoingUp = false;
+            isGoingDown = false;
+
+        }
+    }
+
+    private void onSwipeLeft() {
+        if (!isGoingLeft && !isGoingRight) {
+
+            isGoingRight = false;
+            isGoingLeft = true;
+            isGoingUp = false;
+            isGoingDown = false;
+
+        }
+    }
+
+    private void onSwipeUp() {
+        if (!isGoingDown && !isGoingUp) {
+
+            isGoingRight = false;
+            isGoingLeft = false;
+            isGoingUp = true;
+            isGoingDown = false;
+
+        }
+    }
+
+    private void onSwipeDown() {
+        if (!isGoingDown && !isGoingUp) {
+
+            isGoingRight = false;
+            isGoingLeft = false;
+            isGoingUp = false;
+            isGoingDown = true;
+
+        }
+    }
+
+    private void clickLeft() {
+        if (!clickLeft && !clickRight) {
+
+            clickLeft = true;
+            clickRight = false;
+            clickUp = false;
+            clickDown = false;
+
+        }
+    }
+
+    private void clickRight() {
+        if (!clickRight && !clickLeft) {
+
+            clickLeft = false;
+            clickRight = true;
+            clickUp = false;
+            clickDown = false;
+
+        }
+    }
+
+    private void ClickUp() {
+        if (!clickUp && !clickDown) {
+
+            clickLeft = false;
+            clickRight = false;
+            clickUp = true;
+            clickDown = false;
+
+        }
+    }
+
+    private void clickDown() {
+        if (!clickDown && !clickUp) {
+
+            clickLeft = false;
+            clickRight = false;
+            clickUp = false;
+            clickDown = true;
+
+        }
     }
 
 }
